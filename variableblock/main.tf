30900aba5b51dc55b07a1f1aa2d9bc95430577f4
Amit_Divate <adivate49@gmail.com>
2025-12-03 16:22:34 +0530

    changes new

--- variableblock/main.tf
+++ variableblock/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "webserver" {
-    instance_type = "t3.micro"
-    ami = "ami-02b8269d5e85954ef"
-    key_name = "Oct30"
-    vpc_security_group_ids = [ "sg-0a1badee4156834ca" ]
-    disable_api_termination = false
-    count = 2
+    instance_type = var.webserver_instance_type
+    ami = var.webserver_ami
+    key_name = var.webserver_key_name
+    vpc_security_group_ids = [ var.webserver_vpc_security_group_ids ]
+    disable_api_termination = var.webserver_disable_api_termination
+    count = var.webserver_count
 }
---
+++ variableblock/variable.tf
@@ -0,0 +1,18 @@
+variable "webserver_instance_type" {
+    default = "t3.small"
+}
+variable "webserver_ami" {
+    default = "ami-02b8269d5e85954ef"
+}
+variable "webserver_key_name" {
+  default = "Oct30"
+}
+variable "webserver_vpc_security_group_ids" {
+  default = "sg-0a1badee4156834ca"
+}
+variable "webserver_disable_api_termination" {
+  default = false
+}
+variable "webserver_count" {
+  default = 2 
+}
