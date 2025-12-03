f1895d45886d143ad6abe03d4d8c632a2ed2585c
Amit_Divate <adivate49@gmail.com>
2025-12-03 16:39:10 +0530

    changes in variable

--- variableblock/profile.tf
+++ variableblock/profile.tf
@@ -1,4 +1,4 @@
 provider "aws" {
-    region = "ap-south-1"
-    profile = "configs"
+  region  = "ap-south-1"
+  profile = "configs"
 }
--- variableblock/variable.tf
+++ variableblock/variable.tf
@@ -1,8 +1,8 @@
 variable "webserver_instance_type" {
-    default = "t3.small"
+  default = "t3.small"
 }
 variable "webserver_ami" {
-    default = "ami-02b8269d5e85954ef"
+  default = "ami-02b8269d5e85954ef"
 }
 variable "webserver_key_name" {
   default = "Oct30"
@@ -11,8 +11,8 @@ variable "webserver_vpc_security_group_ids" {
   default = "sg-0a1badee4156834ca"
 }
 variable "webserver_disable_api_termination" {
-  default = false
+  default = true
 }
 variable "webserver_count" {
-  default = 2 
+  default = 2
 }
