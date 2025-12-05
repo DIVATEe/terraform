ab66507aa060d0eb4fe945c3372b6950644df866
Amit_Divate <adivate49@gmail.com>
2025-12-05 16:19:56 +0530

    new

--- datablock/main.tf
+++ datablock/main.tf
@@ -10,24 +10,28 @@ resource "aws_instance" "webserver" {
                  #!/bin/bash
                  sudo apt update -y
                  sudo apt install nginx -y
-                 # Add Docker's official GPG key:
-                 sudo apt update
-                 sudo apt install ca-certificates curl
-                 sudo install -m 0755 -d /etc/apt/keyrings
-                 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-                 sudo chmod a+r /etc/apt/keyrings/docker.asc
+                 # Create directory for Docker key
+                 install -m 0755 -d /etc/apt/keyrings
 
-                 # Add the repository to Apt sources:
-                 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+                 # Add Docker's official GPG key
+                 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+                 chmod a+r /etc/apt/keyrings/docker.asc
+
+                 # Add the Docker repository to Apt sources
+                 cat <<EOF >/etc/apt/sources.list.d/docker.sources
                  Types: deb
                  URIs: https://download.docker.com/linux/ubuntu
-                 Suites: $(. /etc/os-release && echo "${"UBUNTU_CODENAME:-$VERSION_CODENAME"}")
+                 Suites: $(. /etc/os-release && echo "$"{UBUNTU_CODENAME:-$VERSION_CODENAME}"")
                  Components: stable
                  Signed-By: /etc/apt/keyrings/docker.asc
+                 apt-get update -y
 
-                 sudo apt update -y
+                 # Install Docker and plugins
+                 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-                 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+                 # Enable and start Docker
+                 systemctl enable docker
+                 systemctl start docker
                  EOF
 
 
