6b600a72968a67079adcb16ad848bc7119157d0d
Amit_Divate <adivate49@gmail.com>
2025-12-05 15:52:28 +0530

    changes eof

--- datablock/main.tf
+++ datablock/main.tf
@@ -5,6 +5,30 @@ resource "aws_instance" "webserver" {
   vpc_security_group_ids  = [var.webserver_vpc_security_group_ids, aws_security_group.webserver_sg.id, data.aws_security_group.data_webserver_sg.id]
   disable_api_termination = var.webserver_disable_api_termination
   #count                   = var.webserver_count
+
+  user_data = <<-EOF
+                 #!bin/bash
+                 sudo apt update -y
+                 sudo apt install nginx -y
+                 # Add Docker's official GPG key:
+                 sudo apt update
+                 sudo apt install ca-certificates curl
+                 sudo install -m 0755 -d /etc/apt/keyrings
+                 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+                 sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+                 # Add the repository to Apt sources:
+                 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+                 Types: deb
+                 URIs: https://download.docker.com/linux/ubuntu
+                 Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME-VERSION_CODENAME}")
+                 Components: stable
+                 Signed-By: /etc/apt/keyrings/docker.asc
+
+                 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+                 EOF
+
+
 }
 
 resource "aws_security_group" "webserver_sg" {
