a93c1a128d15cb0505a5165e14a78b93f44cd901
Amit_Divate <adivate49@gmail.com>
2025-12-05 16:38:00 +0530

    html page

--- datablock/main.tf
+++ datablock/main.tf
@@ -6,33 +6,20 @@ resource "aws_instance" "webserver" {
   disable_api_termination = var.webserver_disable_api_termination
   #count                   = var.webserver_count
 
-  user_data = <<-EOF
+  user_data = <<-EOT
                  #!/bin/bash
                  sudo apt update -y
                  sudo apt install nginx -y
-                 # Create directory for Docker key
-                 install -m 0755 -d /etc/apt/keyrings
-
-                 # Add Docker's official GPG key
-                 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-                 chmod a+r /etc/apt/keyrings/docker.asc
-
-                 # Add the Docker repository to Apt sources
-                 cat <<EOF >/etc/apt/sources.list.d/docker.sources
-                 Types: deb
-                 URIs: https://download.docker.com/linux/ubuntu
-                 Suites: $(. /etc/os-release && echo "$"{UBUNTU_CODENAME:-$VERSION_CODENAME}"")
-                 Components: stable
-                 Signed-By: /etc/apt/keyrings/docker.asc
-                 apt-get update -y
-
-                 # Install Docker and plugins
-                 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-                 # Enable and start Docker
-                 systemctl enable docker
-                 systemctl start docker
+                 cat <<-EOF >/var/www/html/index.html
+                 <html>
+                  <h1> hello Amit B-61 <h1>
+                  <h2> from terraform user data script <h2>
+                 <html>
                  EOF
+                 systemctl restart nginx
+                 systemctl enable nginx
+
+                 EOT
 
 
 }
