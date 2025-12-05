b80d344440ddfae18238e19068010ef10f75c724
Amit_Divate <adivate49@gmail.com>
2025-12-05 15:12:52 +0530

    added files to outputblock

--- datablock/main.tf
+++ datablock/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "webserver" {
   instance_type           = var.webserver_instance_type
   ami                     = var.webserver_ami
   key_name                = var.webserver_key_name
-  vpc_security_group_ids  = [var.webserver_vpc_security_group_ids]
+  vpc_security_group_ids  = [var.webserver_vpc_security_group_ids , aws_security_group.webserver_sg.id , data.aws_security_group.data_webserver_sg.id]
   disable_api_termination = var.webserver_disable_api_termination
   #count                   = var.webserver_count
 }
@@ -47,4 +47,17 @@ output "webserver_sg_arn" {
 
 output "webserver_sg_id" {
   value =  aws_security_group.webserver_sg.id
+}
+
+
+data "aws_security_group" "data_webserver_sg" {
+  name =  launch-wizard-1
+}
+
+data "aws_ami" "data_webserver_ami" {
+  name = "al2023-ami-2023.9.20251117.1-kernel-6.1-x86_64"
+}
+
+data "aws_instance" "data_webserver_instance" {
+  instance_id = i-09bbf61f4f0ef246f
 }
