869cd778731c548d860bc86b758f96d50c505a96
Amit_Divate <adivate49@gmail.com>
2025-12-08 15:46:54 +0530

    new

--- ASGandLB/main.tf
+++ ASGandLB/main.tf
@@ -4,3 +4,11 @@ resource "aws_instance" "web" {
   instance_type          = var.web_instance_type
   vpc_security_group_ids = [var.web_vpc_security_group_ids]
 }
+
+output "web_publicip" {
+  value = aws_instance.web.public_ip
+}
+
+output "web_publicdns" {
+  value = aws_instance.web.public_dns
+}
